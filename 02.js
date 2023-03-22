e97339bcf065986c8a0a71fed95303f2daa3e044
gerius82 <germaniusto@hotmail.com>
2023-03-22 01:02:25 -0300

    Challenge sim 02

--- 02.js
+++ 02.js
@@ -12,6 +12,15 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  let largo = 0;
+  let masLargo = "";
+  for ( let i = 0 ; i < strings.length ; i++){
+    if (strings[i].length > largo){
+      largo = strings[i].length
+      masLargo = strings[i]
+    }
+  }
+  return masLargo
 }
 
 // No modifiques nada debajo de esta linea //
